40418d0f762f05a5097529a1353cfd47eb2b0e0f
Jaymin Patel <jaymin.future365@gmail.com>
2023-03-14 12:14:44 +0530

    Init project

---
+++ src/test/java/dev/jaymin/sfgjms/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jaymin.sfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
